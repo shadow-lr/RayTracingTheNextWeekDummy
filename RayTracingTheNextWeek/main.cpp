5948fd2f78b7caa107fda4dcf7837261784e7441
shadow-lr <shaodow_lr@163.com>
2021-06-20 17:03:50 +0800

    Finish texture

--- RayTracingTheNextWeek/main.cpp
+++ RayTracingTheNextWeek/main.cpp
@@ -84,8 +84,8 @@ hittable_list random_scene() {
 //    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
 //    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
 
-    auto checker = make_shared<checker_texture>(color(0.2,0.3,0.1),color(0.9,0.9,0.9));
-    world.add(make_shared<sphere>(point3(0,-1000,0),1000,make_shared<lambertian>(checker)));
+    auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(checker)));
 
     for (int a = -11; a < 11; a++) {
         for (int b = -11; b < 11; b++) {
